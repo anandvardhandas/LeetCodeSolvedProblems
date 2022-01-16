cc1ad9c41f012c4508dec0c7659aa11cd34590e6
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-01-16 19:28:01 +0530

    Time: 1176 ms (71.60%), Space: 57.7 MB (23.46%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cs
@@ -0,0 +1,57 @@
+public class Solution {
+    public char[][] RotateTheBox(char[][] box) {
+        int m = box.Length, n = box[0].Length;
+        
+        char[,] result = new char[n,m];
+        for(int i = 0; i < result.GetLength(0); i++){
+            for(int j = 0; j < result.GetLength(1); j++){
+                result[i,j] = '.';
+            }
+        }
+        
+        int col = 0, row = 0;
+        for(int i = m-1; i >= 0; i--){
+            row = n-1;
+            int swapIndex = n-1;
+            for(int j = n-1; j >=0; j--){
+                if(box[i][j] == '*'){
+                    result[row,col] = '*';
+                    swapIndex = j-1;
+                }
+                else if(box[i][j] == '.'){
+                    result[row,col] = '.';
+                    //swapIndex = j;
+                }
+                else{
+                    //swap with swap index
+                    char temp = result[swapIndex, col];
+                    result[swapIndex, col] = '#';
+                    //result[row, col] = '.';
+                    swapIndex--;
+                }
+                
+                
+                row--;
+            }
+            
+            col++;
+        }
+        
+        return ConvertTo2DArray(result);
+    }
+    
+    private char[][] ConvertTo2DArray(char[,] arr)
+    {
+        char[][] result = new char[arr.GetLength(0)][];
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            result[i] = new char[arr.GetLength(1)];
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                result[i][j] = arr[i, j];
+            }
+        }
+
+        return result;
+    }
+}
