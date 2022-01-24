8117a7b1d8d207d850db6838b7ca96f8c9c973c9
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-01-24 18:21:09 +0530

    Time: 10 ms (98.82%), Space: 40.8 MB (55.07%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        
+        if(grid[0][0] == 1 || grid[n-1][n-1] == 1)
+            return -1;
+        
+        //As we are travelling in all directions, we cannot go with dynamic programming approach as we havent travveled all the cells for which the current cells is dependent to calculate result, 
+        // Hence we are doing BFS approach
+        Queue<int[]> que = new ArrayDeque<>();
+        que.add(new int[] { 0,0 });
+        grid[0][0] = 1;
+        
+        while(!que.isEmpty()){
+            int[] node = que.remove();
+            int row = node[0], col = node[1];
+            int dist =  grid[row][col];
+            
+            if(row == n-1 && col == n-1)
+                return dist;
+            
+            for(int[] dir : Directions){
+                int nrow = dir[0]+row, ncol = dir[1]+col;
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < n && grid[nrow][ncol] == 0){
+                    que.add(new int[] { nrow, ncol });
+                    grid[nrow][ncol] = dist+1;
+                }
+                    
+            }
+        }
+        
+        return -1;
+    }
+    
+    private int[][] Directions = new int[][]{
+        new int[] { 1,0 },
+        new int[] { -1,0 },
+        new int[] { 0,1 },
+        new int[] { 0,-1 },
+        
+        new int[] { -1,1 },
+        new int[] { 1,1 },
+        new int[] { 1,-1 },
+        new int[] { -1,-1 },
+    };
+}
