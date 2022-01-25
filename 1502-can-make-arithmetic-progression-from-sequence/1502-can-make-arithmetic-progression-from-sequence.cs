f8bf81015a170e25f953a80ad00304f2a369133f
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-01-25 22:23:00 +0530

    Time: 116 ms (36.36%), Space: 38.1 MB (96.97%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cs
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cs
@@ -1,10 +1,24 @@
 public class Solution {
     public bool CanMakeArithmeticProgression(int[] arr) {
-        Array.Sort(arr);
-        int diff = arr[1] - arr[0];
-        for(int i = 2; i < arr.Length; i++){
-            if(arr[i] - arr[i-1] != diff)
+        // Airthmetic Progression: a, a+d, a+2d, a+3d, .... a+(n-1)d
+        // min = a, max = a+(n-1)d, hence d = (max-min)/(n-1)
+        int len = arr.Length;
+        int max = int.MinValue, min = int.MaxValue;
+        Dictionary<int,int> map = new Dictionary<int,int>();
+        for(int i = 0; i < len; i++){
+            max = Math.Max(max, arr[i]);
+            min = Math.Min(min, arr[i]);
+            if(!map.ContainsKey(arr[i]))
+                map.Add(arr[i],arr[i]);
+        }
+        
+        int d = (max-min)/(len-1);
+        int start = min;
+        for(int i = 0; i < len; i++){
+            if(!map.ContainsKey(start))
                 return false;
+            
+            start += d;
         }
         
         return true;
