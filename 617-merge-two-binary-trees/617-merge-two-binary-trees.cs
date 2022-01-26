fa169f24b106f78fcb80c3e7a87b9e693d5cff5a
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-01-26 18:17:04 +0530

    Time: 143 ms (35.84%), Space: 38.3 MB (63.03%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cs
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cs
@@ -13,9 +13,6 @@
  */
 public class Solution {
     public TreeNode MergeTrees(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null)
-            return null;
-        
         if(root1 == null) return root2;
         if(root2 == null) return root1;
         
