761d35cc69c3f1a1c8e11487d66958e9b1ea0f5c
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-01-29 03:35:08 +0530

    Time: 273 ms (10.22%), Space: 44.4 MB (14.88%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cs
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cs
@@ -11,112 +11,40 @@ public class Solution {
             }
         }
         
-        PriorityQueue pq = new PriorityQueue(k);
+        //using bucket sort algorithm
+        //create bucket of size n as max frequency of any number can be is n
+        List<int>[] bucket = new List<int>[len+1];
         foreach(var item in map){
-            if(pq.count < k){
-                pq.Enqueue(new Node(item.Key, item.Value));
+            var lst = bucket[item.Value];
+            if(lst == null){
+                lst = new List<int>() { item.Key };
             }
             else{
-                Node minElement = pq.Peek();
-                if(item.Value > minElement.freq){
-                    pq.Dequeue();
-                    pq.Enqueue(new Node(item.Key, item.Value));
-                }
+                lst.Add(item.Key);
             }
+            
+            bucket[item.Value] = lst;
         }
         
         List<int> result = new List<int>();
-        while(pq.count > 0){
-            result.Add(pq.Dequeue().num);
+        int count = 0, j = len;
+        while(count < k){
+            while(bucket[j] == null){
+                j--;
+            }
+            
+            List<int> temp = bucket[j];
+            foreach(int item in temp){
+                result.Add(item);
+                count++;
+                if(count == k)
+                    break;
+            }
+            
+            j--;
         }
         
         return result.ToArray();
     }
 }
 
-public class Node
-{
-    public int num;
-    public int freq;
-    
-    public Node(int _num, int _freq)
-    {
-        num = _num;
-        freq = _freq;
-    }
-}
-
-//Min heap
-public class PriorityQueue
-{
-    public Node[] nums;
-    public int count;
-    private int cap;
-    public PriorityQueue(int _capacity)
-    {
-        cap = _capacity;
-        nums = new Node[cap];
-    }
-
-    public Node Peek()
-    {
-        if (count > 0)
-            return nums[0];
-        return null;
-    }
-
-    public void Enqueue(Node node)
-    {
-        if (count < cap)
-        {
-            nums[count] = node;
-            count++;
-            int i = count - 1;
-            while (i > 0 && nums[i].freq < nums[(i - 1) / 2].freq)
-            {
-                //Swap
-                Node temp = nums[i];
-                nums[i] = nums[(i - 1) / 2];
-                nums[(i - 1) / 2] = temp;
-
-                i = (i - 1) / 2;
-            }
-        }
-    }
-
-    public Node Dequeue()
-    {
-        if (count > 0)
-        {
-            Node result = nums[0];
-            nums[0] = nums[count - 1];
-            count--;
-
-            int i = 0;
-            while (2 * i + 1 < count)
-            {
-                int greater = 2 * i + 1;
-                if (2 * i + 2 < count && nums[2 * i + 2].freq < nums[greater].freq)
-                {
-                    greater = 2 * i + 2;
-                }
-
-                if (nums[greater].freq < nums[i].freq)
-                {
-                    //swap
-                    Node temp = nums[i];
-                    nums[i] = nums[greater];
-                    nums[greater] = temp;
-
-                    i = greater;
-                }
-                else
-                    break;
-            }
-
-            return result;
-        }
-
-        return null;
-    }
-}
