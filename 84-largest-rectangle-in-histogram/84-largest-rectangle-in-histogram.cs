10187acf3e8d6ba5f5563edca8dcce69ec5a283e
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-01-30 00:01:33 +0530

    Time: 948 ms (5.23%), Space: 48.6 MB (6.15%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cs
@@ -0,0 +1,61 @@
+public class Solution {
+    public int LargestRectangleArea(int[] heights) {
+        int len = heights.Length;
+        
+        int[] left = new int[len];
+        Stack<int> st = new Stack<int>();
+        left[0] = -1;
+        st.Push(0);
+        for(int i = 1; i < len; i++){
+            while(st.Count > 0 && heights[i] <= heights[st.Peek()]){
+                st.Pop();
+            }
+            
+            if(st.Count == 0)
+                left[i] = -1;
+            else
+                left[i] = st.Peek();
+            
+            st.Push(i);
+        }
+        
+        int[] right = new int[len];
+        st = new Stack<int>();
+        st.Push(len-1);
+        right[len-1] = len;
+        for(int i = len-2; i >= 0; i--){
+            while(st.Count > 0 && heights[i] <= heights[st.Peek()]){
+                st.Pop();
+            }
+            
+            if(st.Count == 0)
+                right[i] = len;
+            else
+                right[i] = st.Peek();
+            
+            st.Push(i);
+        }
+        
+        foreach(var item in left){
+            Console.WriteLine(item);
+        }
+        
+        Console.WriteLine("------");
+        
+        foreach(var item in right){
+            Console.WriteLine(item);
+        }
+        
+        int maxArea = 0;
+        Console.WriteLine("------");
+        for(int i = 0; i < len; i++){
+            int width = (i-left[i]) + (right[i]-i) - 1;
+            int currArea = width * heights[i];
+            Console.WriteLine(currArea);
+            maxArea = Math.Max(maxArea, currArea);
+        }
+        
+        
+        return maxArea;;
+    }
+}
