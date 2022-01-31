d9db5da9740e2195608f1c3bdcf1e1f9e8f9ba94
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-01-31 23:09:06 +0530

    Time: 105 ms (49.02%), Space: 36.5 MB (47.06%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cs
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cs
@@ -1,40 +1,34 @@
 public class Solution {
     public bool CheckValidString(string s) {
-        int[,] dp = new int[s.Length+1,s.Length+1];
-        for(int i = 0; i < s.Length+1; i++){
-            for(int j = 0; j < s.Length+1; j++){
-                dp[i,j] = -1;
+        //greedy
+        
+        int openbracketmin = 0, openbracketmax = 0;
+        foreach(char c in s){
+            if(c == '('){
+                openbracketmin++;
+                openbracketmax++;
             }
+            else if(c == ')'){
+                openbracketmin--;
+                openbracketmax--;
+            }
+            else{
+                openbracketmin--;
+                openbracketmax++;
+            }
+            
+            if(openbracketmax < 0)
+                return false;
+            
+            if(openbracketmin < 0)
+                openbracketmin = 0;
         }
         
-        return Helper(s, 0, 0, dp);
+        if(openbracketmin == 0)
+            return true;
+        
+        return false;
     }
     
-    private bool Helper(string s, int index, int openbracketcount, int[,] dp){
-        if(index == s.Length){
-            if(openbracketcount == 0)
-                return true;
-            else
-                return false;
-        }
-        
-        if(openbracketcount < 0)
-            return false;
-        
-        if(dp[index,openbracketcount] >= 0)
-            return dp[index,openbracketcount] == 1;
-        
-        bool result = false;
-        
-        if(s[index] == '(')
-            result = Helper(s, index+1, openbracketcount+1, dp);
-        else if(s[index] == ')')
-            result = Helper(s, index+1, openbracketcount-1, dp);
-        else{
-            result = Helper(s, index+1, openbracketcount, dp) || Helper(s, index+1, openbracketcount+1, dp) || Helper(s, index+1, openbracketcount-1, dp);
-        }
-        
-        dp[index,openbracketcount] = result ? 1 : 0;
-        return result;
-    }
+    
 }
