bc9d73d1a22ad0ca2908b4aa6a818c1f507babdb
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-07 10:57:38 +0530

    Time: 53 ms (34.37%), Space: 26.7 MB (65.62%) - LeetHub

---
+++ 264-ugly-number-ii/264-ugly-number-ii.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public int NthUglyNumber(int n) {
+        if(n == 1)
+            return 1;
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        int p2 = 0; 
+        int p3 = 0; 
+        int p5 = 0;
+        
+        for(int i = 1; i < n; i++){
+            int mult2 = dp[p2] * 2;
+            int mult3 = dp[p3] * 3;
+            int mult5 = dp[p5] * 5;
+            
+            //calculate minimum of these 3
+            int min = Math.Min(mult2, Math.Min(mult3,mult5));
+            dp[i] = min;
+            
+            if(mult2 == min)
+                p2++;
+            if(mult3 == min)
+                p3++;
+            if(mult5 == min)
+                p5++;
+        }
+        
+        return dp[n-1];
+    }
+}
