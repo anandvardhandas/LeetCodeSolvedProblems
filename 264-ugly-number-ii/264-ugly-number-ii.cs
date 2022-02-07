6fcbfcb08f4a1ce674b355b4234a642e66e89a4e
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-07 10:58:43 +0530

    Time: 28 ms (96.88%), Space: 27.7 MB (44.79%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cs
+++ 264-ugly-number-ii/264-ugly-number-ii.cs
@@ -4,6 +4,7 @@ public class Solution {
             return 1;
         int[] dp = new int[n+1];
         dp[0] = 1;
+        //three pointers to keep track of multiples of 2,3 & 5
         int p2 = 0; 
         int p3 = 0; 
         int p5 = 0;
@@ -13,10 +14,11 @@ public class Solution {
             int mult3 = dp[p3] * 3;
             int mult5 = dp[p5] * 5;
             
-            //calculate minimum of these 3
+            //calculate minimum of these 3 for the next ugly number
             int min = Math.Min(mult2, Math.Min(mult3,mult5));
             dp[i] = min;
             
+            //incrementing the pointers
             if(mult2 == min)
                 p2++;
             if(mult3 == min)
