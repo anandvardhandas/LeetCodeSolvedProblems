6ad764b9fb3f330497f61675f57d33afd995fcc8
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-08 13:33:36 +0530

    Time: 89 ms (48.34%), Space: 127.6 MB (52.74%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.java
@@ -0,0 +1,101 @@
+class LRUCache {
+    public HashMap<Integer,Node> map;
+    public int cap;
+    public int size;
+    public DList list;
+    public LRUCache(int capacity) {
+        map = new HashMap<>();
+        cap = capacity;
+        size = 0;
+        list = new DList();
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)){
+            Node node = map.get(key);
+            
+            list.Remove(node);
+            list.Add(node);
+            
+            map.put(key, node);
+            
+            return node.val;
+        }
+        else
+            return -1;
+    }
+    
+    public void put(int key, int value) {
+        if(map.containsKey(key)){
+           Node node = map.get(key);
+            node.val = value;
+            
+            list.Remove(node);
+            list.Add(node);
+            
+            map.put(key, node);
+        }
+        else{
+            if(size == cap){
+                Node removed = list.RemoveLast();
+                size--;
+                map.remove(removed.key);
+            }
+            
+            Node node = new Node();
+            node.key = key;
+            node.val = value;
+            
+            list.Add(node);
+            
+            map.put(key, node);
+            
+            size++;
+        }
+    }
+}
+
+ class DList{
+    public Node head;
+    public Node tail;
+    
+    public DList(){
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+    }
+    
+    public void Add(Node node){
+        node.next = head.next;
+        node.prev = head;
+        head.next.prev = node;
+        head.next = node;
+    }
+    
+    public void Remove(Node node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    
+    public Node RemoveLast(){
+        Node node = tail.prev;
+        Remove(tail.prev);
+        return node;
+    }
+    
+}
+
+ class Node{
+    public int key;
+    public int val;
+    public Node next;
+    public Node prev;
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
