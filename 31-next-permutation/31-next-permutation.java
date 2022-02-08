33a39a1b926a48ed77d6ae005008973d696f6c6c
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-09 02:25:29 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (16.92%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.java
@@ -0,0 +1,69 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        //39845  
+        //approach would be to find from last where the number is decreasing and then find the next greater number from that number
+        int len = nums.length;
+        int i = len-1;
+        while(i > 0 && nums[i] <= nums[i-1]){
+            i--;
+        }
+        
+        //means array is reversed so just reverse it and return
+        if(i == 0){
+            int j = 0, k = len-1;
+            while(j <= k){
+                //Swap
+                Swap(nums, j, k);
+                j++;
+                k--;
+            }
+             return;
+        }
+        
+        //find the next highest number than i-1 th number
+        int l = i;
+        int nextHighest = l++;
+        while(l < len){
+            if(nums[l] > nums[i-1] && nums[l] < nums[nextHighest])
+                nextHighest = l;
+            
+            l++;
+        }
+        
+        Swap(nums, i-1, nextHighest);
+        
+        //Sort the remaining part from ith to last
+        Sort(nums, i, len-1);
+        
+    }
+    
+    private void Sort(int[] nums, int low, int hi){
+        if(low < hi){
+            int pindex = Partition(nums, low, hi);
+            Sort(nums, low, pindex-1);
+            Sort(nums, pindex+1, hi);
+        }
+    }
+    
+    private int Partition(int[] nums, int low, int hi){
+        int pindex = low;
+        int pivot = nums[hi];
+        for(int i = low; i < hi; i++){
+            if(nums[i] <= pivot){
+                //swap
+                Swap(nums, i, pindex);
+                pindex++;
+            }
+        }
+        
+        //swap
+        Swap(nums, pindex, hi);
+        return pindex;
+    }
+    
+    private void Swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
