e97946adad026667c9fc6bc19821c5c44a45cb3c
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-08 15:53:17 +0530

    Time: 39 ms (48.37%), Space: 25.3 MB (71.81%) - LeetHub

---
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.cs
@@ -0,0 +1,28 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is lower than the guess number
+ *			      1 if num is higher than the guess number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution : GuessGame {
+    public int GuessNumber(int n) {
+        int low = 1, hi = n;
+        while(low <= hi){
+            int mid = low + (hi-low)/2;
+            int picked = guess(mid);
+            if(picked  == -1){
+                hi = mid-1;
+            }
+            else if(picked == 1){
+                low = mid+1;
+            }
+            else
+                return mid;
+        }
+        
+        return low;
+    }
+}
