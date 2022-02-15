e983e8be2a218f909ffc205e250686c3df2e25fc
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-15 12:57:50 +0530

    Time: 137 ms (33.33%), Space: 37.7 MB (55.56%) - LeetHub

--- 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cs
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cs
@@ -1,17 +1,32 @@
 public class Solution {
     public int TwoSumLessThanK(int[] nums, int k) {
-        Array.Sort(nums);
-        int low = 0, hi = nums.Length-1;
+        int[] map = new int[1001];
+        for(int i = 0; i < nums.Length; i++){
+            map[nums[i]]++;
+        }
+        int low = 1, hi = 1000;
         int maxSum = -1;
-        while(low < hi){
-            int currSum = nums[low] + nums[hi];
-            if(currSum >= k){
+        while(low <= hi){
+            if(map[low] > 0 && map[hi] > 0){
+                if(low == hi && map[low] == 1) // only single frequency of the number hence cannot sum hence breaking out
+                    break;
+                
+                int currSum = low+hi;
+                if(currSum >= k){
+                    hi--;
+                }
+                else{
+                    maxSum = Math.Max(maxSum, currSum);
+                    low++;
+                }
+            }
+            else if(map[hi] == 0){
                 hi--;
             }
             else{
-                maxSum = Math.Max(maxSum, currSum);
                 low++;
             }
+            
         }
         
         return maxSum;
