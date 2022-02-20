d138f48706bc45ad51e7181e763dde0c99f5eff1
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-20 10:29:25 +0530

    Time: 128 ms (100.00%), Space: 45.1 MB (100.00%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cs
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.cs
@@ -9,18 +9,10 @@ public class Solution {
             map[c-97]++;
         }
         
-        
-        int totalchar = 0;
-        for(int i = 0; i < 26; i++){
-            if(map[i] > 0)
-                totalchar++;
-        }
-        
         char[] arr = new char[len];
         
         for(int i = 25; i >= 0; i--){
             int charcount = map[i];
-            bool hadchar = charcount > 0;
             int index = 0;
             while(index < len && charcount > 0){
                 int count = 0;
@@ -37,16 +29,10 @@ public class Solution {
                     index++;
                 }
                 
-                if(count == rl && totalchar == 1 && index < len && arr[index] == 0)
-                    break;
-                
                 if(count == rl){
                     index++;
                 }
             }
-            
-            if(hadchar)
-                totalchar--;
         }
         
         StringBuilder sb = new StringBuilder();
