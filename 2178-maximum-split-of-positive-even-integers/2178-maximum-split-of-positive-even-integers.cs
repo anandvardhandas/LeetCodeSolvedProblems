a223bc9f831cc9f147e62d6f172311975d53b5d0
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-21 01:43:36 +0530

    Time: 271 ms (100.00%), Space: 47 MB (100.00%) - LeetHub

---
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public IList<long> MaximumEvenSplit(long finalSum) {
+        long currsum = finalSum;
+        IList<long> result = new List<long>();
+        
+        if(finalSum % 2 == 1)
+            return result;
+        
+        long num = 2;
+        while(currsum - num >= 0){
+            result.Add(num);
+            currsum -= num;
+            num += 2;
+        }
+        
+        if(currsum == 0)
+            return result;
+        
+        long lastval = result[result.Count-1];
+        //Console.WriteLine(lastval);
+        result.RemoveAt(result.Count-1);
+        //Console.WriteLine(currsum);
+        currsum += lastval;
+        //Console.WriteLine(currsum);
+        result.Add(currsum);
+        return result;
+    }
+}
