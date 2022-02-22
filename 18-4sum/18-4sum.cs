11fae678735a15defeb53e43913bfba6ad390f38
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-22 14:39:42 +0530

    Time: 144 ms (94.75%), Space: 44 MB (47.26%) - LeetHub

--- 18-4sum/18-4sum.cs
+++ 18-4sum/18-4sum.cs
@@ -1,48 +1,46 @@
 public class Solution {
     public IList<IList<int>> FourSum(int[] nums, int target) {
-        int len = nums.Length;
         IList<IList<int>> result = new List<IList<int>>();
-        if(len < 4)
-            return result;
         
         Array.Sort(nums);
-        //-2,-2,-1,0,0,1
-        int i = 0;
-        while(i <= len-4){
-            int j = i+1;
-            while(j <= len-3){
+        int len = nums.Length;
+        //-2,-1,0,0,1,2
+        //-2,-2,-2,-1,0,0,1,2
+        //-2,-1,0,0,1,2
+        for(int i = 0; i <= nums.Length-4; i++){
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
+           
+            for(int j = i+1; j <= nums.Length-3; j++){
+                if(j > i+1 && nums[j] == nums[j-1])
+                    continue;
+                
                 int low = j+1, hi = len-1;
+                int sum = nums[i] + nums[j];
                 while(low < hi){
-                    int sum = nums[i] + nums[j] + nums[low] + nums[hi];
-                    if(sum < target){
-                        low++;
-                    }
-                    else if(sum > target){
-                        hi--;
-                    }
-                    else{
+                    if(sum+nums[low]+nums[hi] == target){
                         result.Add(new List<int>() { nums[i], nums[j], nums[low], nums[hi] });
-                        while(low < hi && nums[low] == nums[low+1]){
+
+                        while(low < len-1 && nums[low] == nums[low+1]){
                             low++;
                         }
-                        
-                        while(hi > low && nums[hi] == nums[hi-1]){
+
+                        while(hi > j+1 && nums[hi] == nums[hi-1]){
                             hi--;
                         }
-                        
+
                         low++;
+                        hi--;
+
+                    }
+                    else if(sum+nums[low]+nums[hi] < target){
+                        low++;
+                    }
+                    else if(sum+nums[low]+nums[hi] > target){
                         hi--;
                     }
                 }
-                
-                while(j < len-3 && nums[j] == nums[j+1]) j++;
-                
-                j++;
             }
-            
-            while(i < len-4 && nums[i] == nums[i+1]) i++;
-            
-            i++;
         }
         
         return result;
