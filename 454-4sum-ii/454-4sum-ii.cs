cbec818efc31c74ac0f1f73318a5599ee3df9d4c
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-22 16:38:53 +0530

    Time: 196 ms (94.99%), Space: 42.3 MB (17.48%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cs
+++ 454-4sum-ii/454-4sum-ii.cs
@@ -1,32 +1,31 @@
 public class Solution {
     public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
-        int n = nums1.Length;
-        int count = 0;
-        //using hashmap or dictionary
         Dictionary<int,int> map = new Dictionary<int,int>();
+        int len = nums1.Length;
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
+        int total = 0;
+        
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
                 int sum = nums3[i]+nums4[j];
                 if(map.ContainsKey(sum)){
                     map[sum]++;
                 }
                 else{
-                    map.Add(sum, 1);
+                    map.Add(sum,1);
                 }
             }
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                int sum = nums1[i] + nums2[j];
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                int sum = nums1[i]+nums2[j];
                 if(map.ContainsKey(-sum)){
-                    count += map[-sum];
+                    total += map[-sum];
                 }
             }
         }
         
-        
-        return count;
+        return total;
     }
 }
