c5c730efa0514b9662d13488a57e5fb70ac41da5
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-02-25 03:20:25 +0530

    Time: 130 ms (45.33%), Space: 37.9 MB (64.40%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cs
@@ -0,0 +1,49 @@
+public class Solution {
+    public int DeleteAndEarn(int[] nums) {
+        SortedDictionary<int,int> map = new SortedDictionary<int,int>();
+        foreach(int num in nums){
+            if(!map.ContainsKey(num)){
+                map.Add(num,num);
+            }
+            else{
+                map[num] += num;
+            }
+        }
+        
+        int[] arr = new int[map.Count];
+        int index = 0;
+        foreach(var item in map){
+            arr[index++] = item.Key;
+        }
+        
+        int[] dp = new int[map.Count+1];
+        
+        return Helper(map, arr, 0, dp);
+    }
+    
+    
+    private int Helper(SortedDictionary<int,int> map, int[] arr, int index, int[] dp){
+        if(index >= map.Count){
+            return 0;
+        }
+        
+        if(dp[index] > 0){
+            return dp[index];
+        }
+        
+        //take the current value and skip next or go to next value
+        int res1 = 0, res2 = 0;
+        if(index < map.Count-1 && arr[index] != arr[index+1]-1){
+            res1 = map[arr[index]] + Helper(map, arr, index+1, dp);
+        }
+        else{
+            res1 = map[arr[index]] + Helper(map, arr, index+2, dp);
+        }
+        
+        res2 = Helper(map, arr, index+1, dp);
+        int result = Math.Max(res1, res2);
+        
+        dp[index] = result;
+        return result;
+    }
+}
