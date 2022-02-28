3fdef49c6cbde9a9e5b1cfd4c67b8a92d5e04266
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-01 03:46:24 +0530

    Time: 321 ms (46.01%), Space: 56.7 MB (96.09%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cs
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cs
@@ -1,9 +1,7 @@
-public class Solution {
-    public long MinimumTime(int[] time, int totalTrips) {
-        int len = time.Length;
-        
+class Solution {
+    public long minimumTime(int[] time, int totalTrips) {
         long lt = 1;
-        long rt = 100000000000000;
+        long rt = 100000000000000L;
         while(lt < rt){
             long mt = (lt + (rt-lt)/2);
             long midtrips = GetTrips(time, mt);
@@ -23,7 +21,7 @@ public class Solution {
     private long GetTrips(int[] time, long sec){
         long total = 0;
         
-        for(int i = 0; i < time.Length; i++){
+        for(int i = 0; i < time.length; i++){
             total = total + (sec/time[i]);
         }
         
