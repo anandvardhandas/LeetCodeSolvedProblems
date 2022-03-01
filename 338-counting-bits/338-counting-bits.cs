83b4f0cb1530077f3d011bcaa7e1c707583ca69e
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-01 11:06:11 +0530

    Time: 152 ms (32.51%), Space: 38.2 MB (36.62%) - LeetHub

--- 338-counting-bits/338-counting-bits.cs
+++ 338-counting-bits/338-counting-bits.cs
@@ -1,24 +1,14 @@
 public class Solution {
     public int[] CountBits(int n) {
         int[] result = new int[n+1];
-        for(int i = 0; i <= n; i++){
-            result[i] = BitCount(i);
+        
+        for(int i = 1; i <= n; i++){
+            //here using the fact that if we right shift any number by 1, then number of 1s will be same as number of 1s n>>1 will have and if the current number is odd then it will have 1 more 1. hence adding num & 1
+            result[i] = result[i>>1] + (i & 1);
         }
         
         return result;
     }
     
-    private int BitCount(int num){
-        int total = 0;
-        
-        while(num > 0){
-            int n = num & 1;
-            if(n == 1)
-                total++;
-            
-            num = num >> 1;
-        }
-        
-        return total;
-    }
+    
 }
