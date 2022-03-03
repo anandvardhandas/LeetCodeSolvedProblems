d66e6de234d0101a198cfcfbec8698d835a1a6e9
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-03 09:51:50 +0530

    Time: 137 ms (33.15%), Space: 36.8 MB (43.65%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cs
+++ 413-arithmetic-slices/413-arithmetic-slices.cs
@@ -1,18 +1,13 @@
 public class Solution {
     public int NumberOfArithmeticSlices(int[] nums) {
         int len = nums.Length;
-        
-        
-        //1,2,3,4
-        //1,1,1,1
-        
         int i = 1;
         int prev = int.MaxValue;
         int count = 0;
         int total = 0;
         while(i < len){
             if(nums[i]-nums[i-1] == prev){
-                count = count + 1;
+                count++;
                 total += count;
             }
             else{
