199570c87b6516bea48170440b22aea544eb56ed
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-09 19:05:16 +0530

    Time: 182 ms (68.99%), Space: 44.4 MB (64.87%) - LeetHub

--- 57-insert-interval/57-insert-interval.cs
+++ 57-insert-interval/57-insert-interval.cs
@@ -1,33 +1,34 @@
 public class Solution {
     public int[][] Insert(int[][] intervals, int[] newInterval) {
-        List<int[]> result = new List<int[]>();
         int len = intervals.Length;
+        List<int[]> result = new List<int[]>();
         
+        //adding all intervals below start of new interval
         int i = 0;
-        
-        //Adding all intervals starting before new interval
-        while(i < len && intervals[i][1] < newInterval[0]){
-            result.Add(intervals[i]);
-            i++;
+        for(; i < len; i++){
+            if(intervals[i][1] < newInterval[0])
+                result.Add(intervals[i]);
+            else
+                break;
         }
         
-        int start = newInterval[0];
-        int end = newInterval[1];
-        
-        //merging the intervals in between
-        while(i < len && newInterval[1] >= intervals[i][0]){
-           start = Math.Min(start, intervals[i][0]);
-           end = Math.Max(end, intervals[i][1]);
-           i++;
-        }
-        
-        result.Add(new int[] { start, end });
-        
-        //adding all intervals starting after new intervals
-        while(i < len && intervals[i][0] > newInterval[1]){
-            result.Add(intervals[i]);
-            i++;
-        }
+        //merging in between the intervals
+            int start = newInterval[0];
+            int end = newInterval[1];
+
+            while(i < len && newInterval[1] >= intervals[i][0]){
+                start = Math.Min(intervals[i][0], start);
+                end = Math.Max(end, intervals[i][1]);
+                i++;
+            }
+
+            result.Add(new int[] { start, end });
+
+            //adding all intervals after end of new interval
+            for(; i < len; i++){
+                if(intervals[i][0] > newInterval[1])
+                    result.Add(intervals[i]);
+            }
         
         return result.ToArray();
     }
