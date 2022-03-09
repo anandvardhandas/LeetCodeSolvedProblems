d767f1b8a45bce527db446b76b1e654f977143dc
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-09 17:52:21 +0530

    Time: 172 ms (85.37%), Space: 48.7 MB (6.49%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cs
+++ 56-merge-intervals/56-merge-intervals.cs
@@ -1,24 +1,23 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        int len = intervals.GetLength(0);
-        
-        //Sort Logic
         Array.Sort(intervals, (x,y) => x[0].CompareTo(y[0]));
         
         List<int[]> result = new List<int[]>();
-        int[] merged = new int[] { intervals[0][0], intervals[0][1]};
-        for(int i = 1; i < len; i++){
-            if(intervals[i][0] <= merged[1]){
-                int greater = merged[1] > intervals[i][1] ? merged[1] : intervals[i][1];
-                merged = new int[] { merged[0], greater };
+        
+        int start = intervals[0][0], end = intervals[0][1];
+        
+        for(int i = 1; i < intervals.Length; i++){
+            if(intervals[i][0] <= end){
+                end = Math.Max(end, intervals[i][1]);
             }
             else{
-                result.Add(merged);
-                merged = new int[] { intervals[i][0], intervals[i][1]};
+                result.Add(new int[] { start, end });
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
         
-        result.Add(merged);
+        result.Add(new int[] { start, end });
         
         return result.ToArray();
     }
