e03d570c1ab5f6cb946c5efaac285049b6b7c90a
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-10 10:47:09 +0530

    Time: 188 ms (6.87%), Space: 39.2 MB (66.94%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cs
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cs
@@ -1,40 +1,19 @@
 public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
-        int row = matrix.Length, col = matrix[0].Length;
-        //binary search to decide in which row can target be
-        int low = 0, hi = matrix.Length-1;
-        while(low < hi){
-            int mid = low + (hi-low)/2;
-            
-            if(matrix[mid][0] == target)
-                return true;
-            else if(matrix[mid][0] > target)
-                hi = mid-1;
-            else if(matrix[mid][0] < target && matrix[mid][col-1] < target)
-                low = mid+1;
-            else if(matrix[mid][0] < target && matrix[mid][col-1] >= target){
-                low = mid;
-                break;
-            }
-            else
-                low = mid;
-        }
+        int m = matrix.Length, n = matrix[0].Length;
         
-        //Console.WriteLine(low);
-        //binary search on row
-        int l = 0, r = matrix[0].Length-1;
-        int count = 0;
-        while(l <= r){
-            int m = l + (r-l)/2;
+        int low = 0, hi = m*n-1;
+        while(low <= hi){
+            int mid = low + (hi-low)/2;
+            int row = mid/n;
+            int col = mid%n;
             
-            if(matrix[low][m] == target)
+            if(matrix[row][col] == target)
                 return true;
-            else if(matrix[low][m] > target)
-                r = m-1;
+            else if(matrix[row][col] < target)
+                low = mid+1;
             else
-                l = m+1;
-            
-            count++;
+                hi = mid-1;
         }
         
         return false;
