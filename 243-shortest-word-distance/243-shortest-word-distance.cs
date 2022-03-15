d9c3168680f9bd004a55cdf12b55dd2135608f88
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-15 16:12:40 +0530

    Time: 111 ms (69.31%), Space: 39.1 MB (33.33%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.cs
+++ 243-shortest-word-distance/243-shortest-word-distance.cs
@@ -1,32 +1,23 @@
 public class Solution {
     public int ShortestDistance(string[] wordsDict, string word1, string word2) {
-        Dictionary<string, List<int>> map = new Dictionary<string, List<int>>();
         int len = wordsDict.Length;
-        for(int i = 0; i < len; i++){
-            if(map.ContainsKey(wordsDict[i])){
-                map[wordsDict[i]].Add(i);
-            }
-            else{
-                map.Add(wordsDict[i], new List<int>() { i });
-            }
-        }
-        
-        List<int> lst1 = map[word1];
-        List<int> lst2 = map[word2];
-        
-        int l = 0, r = 0;
-        
+        int i1 = -1, i2 = -1;
         int mindist = int.MaxValue;
-        while(l < lst1.Count && r < lst2.Count){
-            mindist = Math.Min(mindist, Math.Abs(lst1[l]-lst2[r]));
-            if(lst2[r] < lst1[l]){
-                r++;
+        for(int i = 0; i < len; i++){
+            if(wordsDict[i] == word1){
+                i1 = i;
             }
-            else{
-                l++;
+            else if(wordsDict[i] == word2){
+                i2 = i;
+            }
+            
+            if(i1 != -1 && i2 != -1){
+                mindist = Math.Min(mindist, Math.Abs(i1-i2));
+                if(mindist == 1)
+                    return 1;
             }
         }
-        
+            
         return mindist;
     }
 }
