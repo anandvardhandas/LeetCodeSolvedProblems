1991988f85cac96a965390b97751beab29c4cd97
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-19 11:31:43 +0530

    Time: 384 ms (74.32%), Space: 65.4 MB (47.30%) - LeetHub

--- 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cs
+++ 1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1604-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.cs
@@ -1,36 +1,37 @@
 public class Solution {
     public IList<string> AlertNames(string[] keyName, string[] keyTime) {
-        List<string> result = new List<string>();
         
         Dictionary<string,List<int>> map = new Dictionary<string,List<int>>();
-        
+        SortedDictionary<string,string> result = new SortedDictionary<string,string>();
         for(int i = 0; i < keyName.Length; i++){
-            if(map.ContainsKey(keyName[i])){
-                map[keyName[i]].Add(GetTime(keyTime[i]));
-            }
-            else{
-                map.Add(keyName[i], new List<int>() { GetTime(keyTime[i]) });
-            }
-        }
-        
-        foreach(var item in map){
-            List<int> times = item.Value;
-            times.Sort();
-            for(int i = 2; i < times.Count; i++){
-                if((times[i]-times[i-2]) <= 60){
-                    result.Add(item.Key);
-                    break;
+            string name = keyName[i];
+            if(map.ContainsKey(name)){
+                map[name].Add(GetTime(keyTime[i]));
+                
+                List<int> times = map[name];
+                times.Sort();
+                for(int j = 2; j < times.Count; j++){
+                    if(times[j]-times[j-2] <= 60){
+                        if(!result.ContainsKey(name))
+                            result.Add(name,name);
+                    }
                 }
             }
+            else{
+                map.Add(name, new List<int>() { GetTime(keyTime[i]) });
+            }
         }
         
-        result.Sort();
-        return result;
+        List<string> res = new List<string>();
+        foreach(var item in result){
+            res.Add(item.Key);
+        }
+        
+        return res;
     }
     
     private int GetTime(string time){
         string[] times = time.Split(':');
-        int totaltime = 60 * int.Parse(times[0]) + int.Parse(times[1]);
-        return totaltime;
+        return int.Parse(times[0]) * 60 + int.Parse(times[1]);
     }
 }
