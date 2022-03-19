f6a9f291d908fcdb8e52496b2753f8ade3fefbed
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-19 14:43:08 +0530

    Time: 139 ms (24.53%), Space: 38.9 MB (67.55%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cs
@@ -2,33 +2,22 @@ public class Solution {
     public int LongestCommonSubsequence(string text1, string text2) {
         int m = text1.Length, n = text2.Length;
         int[,] dp = new int[m+1,n+1];
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
-                dp[i,j] = -1;
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                int len1 = 0, len2 = 0, len3 = 0;
+                if(text1[i] == text2[j]){
+                    len1 = 1 + dp[i+1,j+1];
+                }
+                else{
+                    len2 = dp[i,j+1];
+                    len3 = dp[i+1,j];
+                }
+
+                int maxlen = Math.Max(len1, Math.Max(len2, len3));
+                dp[i,j] = maxlen;
             }
         }
-        return Helper(text1, text2, 0, 0, dp);
-    }
-    
-    private int Helper(string text1, string text2, int i, int j, int[,] dp){
-        if(i >= text1.Length || j >= text2.Length)
-            return 0;
         
-        if(dp[i,j] >= 0){
-            return dp[i,j];
-        }
-        
-        int len1 = 0, len2 = 0, len3 = 0;
-        if(text1[i] == text2[j]){
-            len1 = 1 + Helper(text1, text2, i+1, j+1, dp);
-        }
-        else{
-            len2 = Helper(text1, text2, i, j+1, dp);
-            len3 = Helper(text1, text2, i+1, j, dp);
-        }
-        
-        int maxlen = Math.Max(len1, Math.Max(len2, len3));
-        dp[i,j] = maxlen;
-        return maxlen;
+        return dp[0,0];
     }
 }
