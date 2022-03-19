983b33293bea444aee1d93bbcbc1401296a0014f
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-19 16:56:07 +0530

    Time: 281 ms (23.42%), Space: 42.8 MB (30.63%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cs
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cs
@@ -1,17 +1,17 @@
 public class Solution {
     public int FindLength(int[] nums1, int[] nums2) {
         int m = nums1.Length, n = nums2.Length;
-        int[,] dp = new int[m+1,n+1];
         int maxlen = 0;
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(nums1[i-1] == nums2[j-1]){
-                    dp[i,j] = 1 + dp[i-1,j-1];
+        int[,] dp = new int[m+1,n+1];
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(nums1[i] == nums2[j]){
+                    dp[i,j] = 1 + dp[i+1,j+1];
                     maxlen = Math.Max(maxlen, dp[i,j]);
                 }
             }
         }
-    
+        
         return maxlen;
     }
 }
