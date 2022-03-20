a0a365bd3e9f1f107378d6eb8383236f695c4922
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-21 02:35:36 +0530

    Time: 106 ms (59.52%), Space: 37 MB (65.48%) - LeetHub

---
+++ 156-binary-tree-upside-down/156-binary-tree-upside-down.cs
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode UpsideDownBinaryTree(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null))
+            return root;
+        
+        TreeNode newroot = UpsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return newroot;
+    }
+}
