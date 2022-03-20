9240a5818bd14ace70a9edb87333a63ba3b98188
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-20 20:12:18 +0530

    Time: 244 ms (23.84%), Space: 42.3 MB (32.56%) - LeetHub

---
+++ 68-text-justification/68-text-justification.cs
@@ -0,0 +1,55 @@
+public class Solution {
+    public IList<string> FullJustify(string[] words, int maxWidth) {
+        List<string> result = new List<string>();
+        int len = words.Length;
+        int i = 0;
+        while(i < len){
+            int wordcount = words[i].Length;
+            int j = i+1;
+            
+            int spacecount = 0;
+            while(j < words.Length && wordcount + spacecount + words[j].Length + 1 <= maxWidth){
+                wordcount += words[j].Length;
+                spacecount++;
+                j++;
+            }
+            
+            int spacesleft = maxWidth-wordcount;
+            
+            int atleast = spacecount == 0 ? 0 : spacesleft/spacecount;
+            int extra = spacecount == 0 ? 0 : spacesleft % spacecount;
+            //last line
+            if(j == words.Length){
+                atleast = 1;
+                extra = 0;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            
+            for(int k = i; k < j; k++){
+                sb.Append(words[k]);
+                if(k == j-1)
+                    break;
+                
+                for(int e = 0; e < atleast; e++){
+                    sb.Append(" ");
+                    if(extra > 0){
+                        sb.Append(" ");
+                        extra--;
+                    }
+                }
+            }
+            
+            while(sb.Length < maxWidth){
+                sb.Append(" ");
+            }
+            
+            result.Add(sb.ToString());
+            
+            i = j;
+        }
+        
+        
+        return result;
+    }
+}
