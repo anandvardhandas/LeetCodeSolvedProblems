a46c461ca03f96d99efef5f6abd2030243249754
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-20 16:23:11 +0530

    Time: 320 ms (75.36%), Space: 44.5 MB (39.13%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cs
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cs
@@ -10,38 +10,26 @@ public class Solution {
             map[item]++;
         }
         
-        foreach(var item in map){
-            //Console.WriteLine(item);
-        }
-        
         int max = 0;
         int maxnum = -1;
         for(int i = 0; i < map.Length; i++){
-            //Console.WriteLine(map[i]);
             if(map[i] > max){
                 max = map[i];
                 maxnum = i;
             }
         }
-        //Console.WriteLine(maxnum);
-        int topscount = 0;
+        
+        int topscount = 0, botscount = 0;
         for(int i = 0; i < tops.Length; i++){
             if(tops[i] == maxnum)
                 topscount++;
-        }
-        
-        int botscount = 0;
-        for(int i = 0; i < bottoms.Length; i++){
             if(bottoms[i] == maxnum)
                 botscount++;
-        }
-        
-        for(int i = 0; i < tops.Length; i++){
+            
             if(tops[i] != maxnum && bottoms[i] != maxnum)
                 return -1;
         }
         
-        int greater = Math.Max(topscount,botscount);
-        return tops.Length-greater;
+        return Math.Min(tops.Length-topscount,tops.Length-botscount);
     }
 }
