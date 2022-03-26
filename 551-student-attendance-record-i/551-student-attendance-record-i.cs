05cbb0ecb8c67cc9c87ec63d192718276ebe70ce
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-26 19:51:53 +0530

    Time: 102 ms (52.08%), Space: 36.7 MB (61.11%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.cs
+++ 551-student-attendance-record-i/551-student-attendance-record-i.cs
@@ -1,20 +1,22 @@
 public class Solution {
     public bool CheckRecord(string s) {
+        int len = s.Length;
         int abs = 0, late = 0;
-        for(int i = 0; i < s.Length; i++){
-            if(s[i] == 'A'){
+        for(int i = 0; i < len; i++){
+            if(s[i] == 'P'){
+                late = 0;
+            }
+            else if(s[i] == 'A'){
                 abs++;
                 late = 0;
+                if(abs > 1)
+                    return false;
             }
             else if(s[i] == 'L'){
                 late++;
+                if(late == 3)
+                    return false;
             }
-            else{
-                late = 0;
-            }
-            
-            if(abs > 1 || late > 2)
-                return false;
         }
         
         return true;
