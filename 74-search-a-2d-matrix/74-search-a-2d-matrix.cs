bfda262d70d0b1ca68f7ed19ec10ae2a2532cee5
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-03-30 12:04:37 +0530

    Time: 125 ms (58.66%), Space: 39.4 MB (52.06%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cs
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cs
@@ -2,18 +2,37 @@ public class Solution {
     public bool SearchMatrix(int[][] matrix, int target) {
         int m = matrix.Length, n = matrix[0].Length;
         
-        int low = 0, hi = m*n-1;
+        int top = 0, bot = m-1;
+        int rowtochoose = 0;
+        while(top <= bot){
+            int mid = top + (bot-top)/2;
+            int first = matrix[mid][0], last = matrix[mid][n-1];
+            //Console.WriteLine(mid);
+            if(target >= first && target <= last){
+                rowtochoose = mid;
+                break;
+            }
+            else if(target < first){
+                bot = mid-1;
+            }
+            else if(target > last){
+                top = mid+1;
+            }
+        }
+        
+        int low = 0, hi = n-1;
+        
         while(low <= hi){
             int mid = low + (hi-low)/2;
-            int row = mid/n;
-            int col = mid%n;
-            
-            if(matrix[row][col] == target)
+            if(matrix[rowtochoose][mid] == target){
                 return true;
-            else if(matrix[row][col] < target)
-                low = mid+1;
-            else
+            }
+            else if(target < matrix[rowtochoose][mid]){
                 hi = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
         
         return false;
