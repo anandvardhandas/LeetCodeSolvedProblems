a4e13f8fb2440d7b17ad49540647b4c92a3f4059
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-16 09:46:51 +0530

    Time: 108 ms (85.45%), Space: 44.6 MB (9.09%) - LeetHub

--- 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cs
+++ 538-convert-bst-to-greater-tree/538-convert-bst-to-greater-tree.cs
@@ -13,42 +13,19 @@
  */
 public class Solution {
     
-    private int total;
-    private int curr = 0;
     public TreeNode ConvertBST(TreeNode root) {
-        List<int> list = new List<int>();
-        Inorder(root, list);
-        int[] nums = list.ToArray();
-        int sum = 0;
-        for(int i = 0; i < nums.Length; i++){
-            sum += nums[i];
-        }
-        
-        this.total = sum;
-        
-        Convert(root, list);
-        
+        Inorder(root);
         return root;
     }
     
-    private int index = 0;
-    private void Convert(TreeNode root, List<int> list){
+    private int curr = 0;
+    private void Inorder(TreeNode root){
         if(root == null)
             return;
         
-        Convert(root.left, list);
-        int temp = root.val;
-        root.val = this.total-curr;
-        curr += temp;
-        Convert(root.right, list);
-    }
-    
-    private void Inorder(TreeNode root, List<int> list){
-        if(root == null)
-            return;
-        
-        Inorder(root.left, list);
-        list.Add(root.val);
-        Inorder(root.right, list);
+        Inorder(root.right);
+        root.val = root.val+curr;
+        curr = root.val;
+        Inorder(root.left);
     }
 }
