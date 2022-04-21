b27f88c8076c1114d0a4d859318a88bdeb1fe561
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-21 17:55:04 +0530

    Time: 709 ms (31.81%), Space: 53.6 MB (75.74%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cs
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cs
@@ -5,47 +5,30 @@ public class Solution {
         if(k > len)
             return result.ToArray();
         
-        SortedDictionary<int,int> map = 
-            new SortedDictionary<int,int>(Comparer<int>.Create((x,y) => y.CompareTo(x)));
-        
-        //first create a window of size k
-        for(int i = 0; i < k; i++){
-            if(map.ContainsKey(nums[i])){
-                map[nums[i]]++;
+        int l = 0, r = 0;
+        LinkedList<int> que = new LinkedList<int>();
+        while(r < len){
+            //remove all numbers smaller than rth number
+            while(que.Count > 0 && nums[r] >= nums[que.Last.Value]){
+                que.RemoveLast();
             }
-            else{
-                map.Add(nums[i], 1);
+            
+            que.AddLast(r);
+            
+            // set l
+            l = r - k + 1;
+            
+            if(que.First.Value < l){
+                que.RemoveFirst();
             }
-        }
-        
-        //get max
-         result.Add(map.First().Key);
-        //Console.WriteLine(map.First().Key);
-        
-        // start shifting
-        int l = 0, r = k-1;
-        
-        while(r <= len-2){
-            //shift right
+            
+            if(l >= 0){
+                result.Add(nums[que.First.Value]);
+            }
+            
             r++;
-            if(map.ContainsKey(nums[r])){
-                map[nums[r]]++;
-            }
-            else{
-                map.Add(nums[r], 1);
-            }
-            
-            //shift left
-            map[nums[l]]--;
-            if(map[nums[l]] == 0){
-                map.Remove(nums[l]);
-            }
-            
-            l++;
-            
-            //get max
-            result.Add(map.First().Key);
         }
+        
         
         return result.ToArray();
     }
