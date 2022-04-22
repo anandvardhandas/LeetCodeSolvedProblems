7b572d99c3f875ec92175b84c4647d5b88c3d515
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-23 03:01:22 +0530

    Time: 126 ms (68.26%), Space: 41.2 MB (80.90%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.cs
+++ 766-toeplitz-matrix/766-toeplitz-matrix.cs
@@ -2,30 +2,14 @@ public class Solution {
     public bool IsToeplitzMatrix(int[][] matrix) {
            
         int m = matrix.Length, n = matrix[0].Length;
-        for(int i = 0; i < n; i++){
-            if(!Helper(matrix, m, n, 0, i))
-                return false;
-        }
         
-        for(int i = m-1; i > 0; i--){
-            if(!Helper(matrix, m, n, i, 0))
-                return false;
+        for(int i = 0; i < m-1; i++){
+            for(int j = 0; j < n-1; j++){
+                if(matrix[i][j] != matrix[i+1][j+1])
+                    return false;
+            }
         }
         
         return true;
-        
     }
-    
-    private bool Helper(int[][] matrix, int m, int n, int x, int y){
-        if(x >= m-1 || y >= n-1)
-            return true;
-        
-        if(matrix[x][y] != matrix[x+1][y+1]){
-            return false;
-        }
-        
-        return Helper(matrix, m, n, x+1, y+1);
-    }
-    
-    
 }
