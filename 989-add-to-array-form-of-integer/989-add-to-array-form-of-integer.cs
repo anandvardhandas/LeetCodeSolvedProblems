2c845e583093355531cd689d84f5bbadb3eec8f1
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-23 15:07:32 +0530

    Time: 156 ms (100.00%), Space: 48 MB (17.99%) - LeetHub

---
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public IList<int> AddToArrayForm(int[] num, int k) {
+        List<int> knum = new List<int>();
+        while(k > 0){
+            knum.Add(k%10);
+            k = k/10;
+        }
+        
+        int[] nums = knum.ToArray();
+        Array.Reverse(nums);
+        
+        int i = num.Length-1, j = nums.Length-1;
+        
+        IList<int> result = new List<int>();
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0){
+                sum += num[i];
+                i--;
+            }
+            
+            if(j >= 0){
+                sum += nums[j];
+                j--;
+            }
+            
+            carry = sum/10;
+            sum = sum%10;
+            
+            result.Add(sum);
+        }
+        
+        if(carry > 0){
+            result.Add(carry);
+        }
+        
+        int[] res = result.ToArray();
+        Array.Reverse(res);
+        return res.ToList();
+    }
+}
