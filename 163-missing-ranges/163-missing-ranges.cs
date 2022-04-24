3c4e8b438ae3105ba956fdab29998fcc5decd08e
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-25 00:55:05 +0530

    Time: 140 ms (93.37%), Space: 43.5 MB (6.63%) - LeetHub

--- 163-missing-ranges/163-missing-ranges.cs
+++ 163-missing-ranges/163-missing-ranges.cs
@@ -13,21 +13,8 @@ public class Solution {
             return result;
         }
         
-        int prev = lower;
+        int prev = lower-1;
         for(int i = 0; i < nums.Length; i++){
-            if(i == 0 && nums[i] > prev){
-                int l = prev;
-                int r = nums[i]-1;
-                if(l == r){
-                    result.Add($"{l}");
-                }
-                else{
-                    result.Add($"{l}->{r}");
-                }
-                
-                prev = nums[i];
-            }
-            
             if(nums[i] > prev+1){
                 int l = prev+1;
                 int r = nums[i]-1;
