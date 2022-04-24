9c141063e1a5d00984b20d35c5bb994f3f0ae341
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-25 03:35:09 +0530

    Time: 137 ms (45.90%), Space: 41.1 MB (63.93%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cs
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.cs
@@ -2,14 +2,17 @@ public class Solution {
     public bool CanBeEqual(int[] target, int[] arr) {
         //[1,2,3,4]  [4,1,2,3  1,4,2,3  1,2,4,3  1,2,3,4]
         
-        Array.Sort(target);
-        Array.Sort(arr);
-        int i = 0;
-        while(i < target.Length){
-            if(target[i] != arr[i])
+        int[] map1 = new int[1001];
+        int[] map2 = new int[1001];
+        
+        for(int i = 0; i < target.Length; i++){
+            map1[target[i]]++;
+            map2[arr[i]]++;
+        }
+        
+        for(int i = 1; i <= 1000; i++){
+            if(map1[i] != map2[i])
                 return false;
-            
-            i++;
         }
         
         return true;
