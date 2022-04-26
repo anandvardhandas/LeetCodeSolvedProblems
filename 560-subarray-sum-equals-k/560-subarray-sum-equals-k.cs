dada5ec8074f23f39cab45ffec2a3e7a24ce4107
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-26 23:58:47 +0530

    Time: 126 ms (87.81%), Space: 50 MB (5.67%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public int SubarraySum(int[] nums, int k) {
+        Dictionary<int,int> map = new Dictionary<int,int>();
+        map.Add(0,1);
+        
+        int len = nums.Length;
+        
+        int[] prefix = new int[len];
+        prefix[0] = nums[0];
+        for(int i = 1; i < len; i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        
+        //[2,1,-2,2,1] k = 3
+        //[2,3,1,3,4]
+        //[1,1,1] k = 2
+        //[1,2,3]
+        int total = 0;
+        for(int i = 0; i < len; i++){
+            if(map.ContainsKey(prefix[i]-k)){
+                //Console.WriteLine(i);
+                total += map[prefix[i]-k];
+            }
+            
+            if(!map.ContainsKey(prefix[i])){
+                map.Add(prefix[i],1);
+            }
+            else{
+                map[prefix[i]]++;
+            }
+        }
+        
+        return total;
+        
+    }
+}
