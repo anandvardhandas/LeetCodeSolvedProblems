8553975fa564ddf9c25a28de8dd3b9618a84e3d3
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-01 02:19:50 +0530

    Time: 32 ms (46.32%), Space: 26.6 MB (23.51%) - LeetHub

---
+++ 670-maximum-swap/670-maximum-swap.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int MaximumSwap(int num) {
+        if(num <= 9)
+            return num;
+        
+        string numstr = num.ToString();
+        char[] nums = numstr.ToCharArray();
+        Array.Sort(nums, (x,y) => y.CompareTo(x));
+        foreach(char c in nums){
+            //Console.WriteLine(c);
+        }
+        int i = 0;
+        while(i<nums.Length){
+            if(nums[i] == numstr[i]){
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+       // Console.WriteLine(i);
+        char[] result = numstr.ToCharArray();
+        if(i < nums.Length){
+            //find the last positionn of nums[i] in numstr
+            int swapindex = -1;
+            for(int j = numstr.Length-1; j >= 0; j--){
+                if(numstr[j] == nums[i]){
+                    swapindex = j;
+                    break;
+                }
+            }
+
+            //swap i with swapindex in numstr
+            
+            char temp = result[i];
+            result[i] = result[swapindex];
+            result[swapindex] = temp;
+        }
+        
+        return int.Parse(new string(result));
+    }
+}
