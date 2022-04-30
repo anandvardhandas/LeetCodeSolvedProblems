2c614a4d36d9f42ab2349a49e8ef6d51cf3d4e1f
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-04-30 09:55:57 +0530

    Time: 2565 ms (5.14%), Space: 57.1 MB (37.71%) - LeetHub

--- 398-random-pick-index/398-random-pick-index.cs
+++ 398-random-pick-index/398-random-pick-index.cs
@@ -1,25 +1,27 @@
 public class Solution {
-    private int[] map;
+    private int[] nums;
     private Random random;
     public Solution(int[] nums) {
-        map = nums;
-        random = new Random();
+        this.nums = nums;
+        this.random = new Random();
     }
     
     public int Pick(int target) {
+        
+        int index = 0;
         int count = 0;
-        int result = 0;
-        for(int i = 0; i < map.Length; i++){
-            if(map[i] == target){
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] == target){
                 count++;
-                int rand = random.Next(1, count+1);
-                if(rand == 1){
-                    result = i;
+                
+                int rand = random.Next(0,count);
+                if(rand == 0){
+                    index = i;
                 }
             }
         }
         
-        return result;
+        return index;
     }
 }
 
