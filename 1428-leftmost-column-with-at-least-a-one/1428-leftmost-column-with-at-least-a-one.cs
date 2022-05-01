32f1f79fc4d24b21451ff8dc7a8e79ba3c301689
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-01 18:05:10 +0530

    Time: 123 ms (46.26%), Space: 39.8 MB (57.14%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cs
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cs
@@ -9,38 +9,21 @@
 
 class Solution {
     public int LeftMostColumnWithOne(BinaryMatrix binaryMatrix) {
-       IList<int> dims =  binaryMatrix.Dimensions();
-        int row = dims[0], col = dims[1];
+        IList<int> dims = binaryMatrix.Dimensions();
+        int m = dims[0], n = dims[1];
         
-        int min = int.MaxValue;
-        for(int i = 0; i < row; i++){
-            int low = 0, hi = col-1;
-            if(min != int.MaxValue){
-                hi = min-1;
+        int mincol = -1;
+        int row = 0, col = n-1;
+        while(row < m && col >= 0){
+            if(binaryMatrix.Get(row,col) == 1){
+                mincol = col;
+                col--;
             }
-            
-            int ans = int.MaxValue;
-            while(low <= hi){
-                int mid = low + (hi-low)/2;
-                if(binaryMatrix.Get(i,mid) == 1){
-                    ans = mid;
-                    hi = mid-1;
-                }
-                else{
-                    low = mid+1;
-                }
-            }
-            
-            if(ans != int.MaxValue)
-                min = Math.Min(min,ans);
-            if(min == 0){
-                return min;
+            else{
+                row++;
             }
         }
         
-        if(min == int.MaxValue)
-            return -1;
-        
-        return min;
+        return mincol;
     }
 }
