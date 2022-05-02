99543700485d06035e20be02fb913689a8168074
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-02 18:23:44 +0530

    Time: 78 ms (74.39%), Space: 38 MB (22.60%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cs
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cs
@@ -5,14 +5,16 @@ public class Solution {
         int len = s.Length;
         int maxlen = 0;
         int low = 0, hi = 0;
-        
+        int maxfreq = 0;
         while(hi < len){
             if(map.ContainsKey(s[hi]))
                 map[s[hi]]++;
             else
                 map.Add(s[hi], 1);
             
-            while(hi-low+1 - map.Values.Max() > k){
+            maxfreq = Math.Max(maxfreq, map[s[hi]]);
+            
+            while((hi-low+1) - maxfreq > k){
                 map[s[low]]--;
                 
                 low++;
