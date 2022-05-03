1bac81c66aa1450d1fcf6b44c1f2492d821a3099
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-04 02:56:38 +0530

    Time: 135 ms (49.20%), Space: 39.8 MB (32.80%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cs
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cs
@@ -1,101 +1,72 @@
 public class Solution {
     public long SubArrayRanges(int[] nums) {
         int len = nums.Length;
-        Stack<long> st = new Stack<long>();
+        
         long[] ple = new long[len];
         long[] nle = new long[len];
         
-        ple[0] = -1;
-        st.Push(0);
-        for(int i = 1; i < len; i++){
-            while(st.Count > 0 && nums[i] <= nums[st.Peek()]){
-                st.Pop();
+        Stack<Node> st = new Stack<Node>();
+        for(int i = 0; i < len; i++){
+            long count = 1;
+            while(st.Count > 0 && nums[i] <= st.Peek().val){
+                count += st.Pop().dist;
             }
             
-            ple[i] = st.Count == 0 ? -1 : st.Peek();
-            st.Push(i);
+            ple[i] = count;
+            st.Push(new Node(nums[i],count));
         }
         
-        st = new Stack<long>();
-        st.Push(len-1);
-        nle[len-1] = -1;
-        for(int i = len-2; i >= 0; i--){
-            while(st.Count > 0 && nums[i] < nums[st.Peek()]){
-                st.Pop();
+        st = new Stack<Node>();
+        for(int i = len-1; i >= 0; i--){
+            long count = 1;
+            while(st.Count > 0 && nums[i] < st.Peek().val){
+                count += st.Pop().dist;
             }
             
-            nle[i] = st.Count == 0 ? -1 : st.Peek();
-            st.Push(i);
-        }
-        long mintotal = 0;
-        for(int i = 0; i < len; i++){
-            long left = 1, right = 1;
-            
-            if(ple[i] != -1){
-                left = i-ple[i];
-            }
-            else{
-                left = i+1;
-            }
-            
-            if(nle[i] != -1){
-                right = nle[i]-i;
-            }
-            else{
-                right = len-i;
-            }
-            
-            mintotal = mintotal + nums[i] * left * right;
+            nle[i] = count;
+            st.Push(new Node(nums[i],count));
         }
         
         long[] pge = new long[len];
         long[] nge = new long[len];
         
-        pge[0] = -1;
-        st = new Stack<long>();
-        st.Push(0);
-        for(int i = 1; i < len; i++){
-            while(st.Count > 0 && nums[i] >= nums[st.Peek()]){
-                st.Pop();
-            }
-            
-            pge[i] = st.Count == 0 ? -1 : st.Peek();
-            st.Push(i);
-        }
-        
-        st = new Stack<long>();
-        st.Push(len-1);
-        nge[len-1] = -1;
-        for(int i = len-2; i >= 0; i--){
-            while(st.Count > 0 && nums[i] > nums[st.Peek()]){
-                st.Pop();
-            }
-            
-            nge[i] = st.Count == 0 ? -1 : st.Peek();
-            st.Push(i);
-        }
-        long maxtotal = 0;
+        st = new Stack<Node>();
         for(int i = 0; i < len; i++){
-            long left = 1, right = 1;
-            
-            if(pge[i] != -1){
-                left = i-pge[i];
-            }
-            else{
-                left = i+1;
+            long count = 1;
+            while(st.Count > 0 && nums[i] >= st.Peek().val){
+                count += st.Pop().dist;
             }
             
-            if(nge[i] != -1){
-                right = nge[i]-i;
-            }
-            else{
-                right = len-i;
+            pge[i] = count;
+            st.Push(new Node(nums[i],count));
+        }
+        
+        st = new Stack<Node>();
+        for(int i = len-1; i >= 0; i--){
+            long count = 1;
+            while(st.Count > 0 && nums[i] > st.Peek().val){
+                count += st.Pop().dist;
             }
             
-            maxtotal = maxtotal + nums[i] * left * right;
+            nge[i] = count;
+            st.Push(new Node(nums[i],count));
+        }
+        
+        long total = 0;
+        for(int i = 0; i < len; i++){
+            total += ((pge[i]*nge[i]) - (ple[i]*nle[i]))*nums[i];
         }
         
         
-        return maxtotal - mintotal;
+        return total;
+    }
+}
+
+public class Node{
+    public long val;
+    public long dist;
+    public Node(long _val, long _dist){
+        val = _val;
+        dist = _dist;
     }
 }
