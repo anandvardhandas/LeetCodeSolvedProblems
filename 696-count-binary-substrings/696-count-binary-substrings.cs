a49f4dfa72d32b5f32dd6d751771bc701069525c
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-05 04:34:21 +0530

    Time: 94 ms (56.07%), Space: 39.9 MB (91.07%) - LeetHub

---
+++ 696-count-binary-substrings/696-count-binary-substrings.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int CountBinarySubstrings(string s) {
+        int len = s.Length;
+        int[] groups = new int[len];
+        groups[0] = 1;
+        int index = 0;
+        for(int i = 1; i < len; i++){
+            if(s[i] == s[i-1]){
+                groups[index]++;
+            }
+            else{
+                index++;
+                groups[index] = 1;
+            }
+        }
+        
+        int result = 0;
+        for(int i = 1; i < index+1; i++){
+            result = result + Math.Min(groups[i], groups[i-1]);
+        }
+        
+        return result;
+    }
+}
