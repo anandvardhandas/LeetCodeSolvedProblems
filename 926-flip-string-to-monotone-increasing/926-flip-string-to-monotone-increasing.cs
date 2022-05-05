ea2b5ef6323bfbe89707e56c2eee3a43001c23f3
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-05 23:35:47 +0530

    Time: 103 ms (40.79%), Space: 41.3 MB (76.53%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cs
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.cs
@@ -1,45 +1,23 @@
 public class Solution {
     public int MinFlipsMonoIncr(string s) {
-        int[,] dp = new int[s.Length,2];
+        int countones = 0;
+        
+        int result = 0;
+        
         for(int i = 0; i < s.Length; i++){
-            dp[i,0] = -1;
-            dp[i,1] = -1;
-        }
-        
-        return Helper(s,0,0, dp);
-    }
-    
-    private int Helper(string s, int index, int prev, int[,] dp){
-        if(index == s.Length){
-            return 0;
-        }
-        
-        if(dp[index,prev] != -1){
-            return dp[index,prev];
-        }
-        
-        int result1 = int.MaxValue, result2 = int.MaxValue, result3 = int.MaxValue;
-        if(prev == 0){
-            result1 = Helper(s, index+1, 0, dp);
-            if(s[index] == '1'){
-                result1 += 1;
+            if(s[i] == '1'){
+                countones++;
             }
-            
-            result2 = Helper(s, index+1, 1, dp);
-            if(s[index] == '0'){
-                result2 += 1;
-            }
-            
-        }
-        else{
-            result3 = Helper(s, index+1, 1, dp);
-            if(s[index] == '0'){
-                result3 += 1;
+            else{
+                result++;//option 1 -> flip this to 1 hence one more step required here in result so far
+                
+                //and if we decide to remain this as zero then we have to make all the previous ones to zero, hence comparing to result and deciding which would be best
+                result = Math.Min(countones, result);
             }
         }
         
-        int result = Math.Min(result1, Math.Min(result2, result3));
-        dp[index,prev] = result;
         return result;
     }
+    
+   
 }
