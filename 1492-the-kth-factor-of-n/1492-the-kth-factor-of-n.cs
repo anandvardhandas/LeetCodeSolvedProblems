f2e79acf8a5e71255028203d805b8865aeaa78c1
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-06 00:22:42 +0530

    Time: 23 ms (82.54%), Space: 25.7 MB (52.38%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cs
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cs
@@ -1,12 +1,24 @@
 public class Solution {
     public int KthFactor(int n, int k) {
-         for(int i = 1; i < Math.Sqrt(n); ++i) 
-                if(n % i== 0 && --k == 0) 
-                    return i;                       
-            for(int i = (int) Math.Sqrt(n); i >= 1; --i) 
-                if(n % (n/i) == 0 && --k == 0) 
-                    return n/i;          
-            return -1;
+        int sqrt = (int) Math.Sqrt(n);
+        for(int i = 1; i < Math.Sqrt(n); i++){
+            if(n%i == 0){
+                k--;
+                if(k == 0){
+                    return i;
+                }
+            }
+        }
+        
+        for(int i = sqrt; i >= 1; i--){
+            if(n%(n/i) == 0){
+                k--;
+                if(k == 0)
+                    return n/i;
+            }
+        }
+        
+        return -1;
         
         /*
             for(int i = 1; i < Math.sqrt(n); ++i) 
