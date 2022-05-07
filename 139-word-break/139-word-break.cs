e5d57496b639a4cad9888232dd88de3688e50719
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-07 21:27:51 +0530

    Time: 122 ms (65.44%), Space: 41.6 MB (58.69%) - LeetHub

--- 139-word-break/139-word-break.cs
+++ 139-word-break/139-word-break.cs
@@ -1,33 +1,32 @@
 public class Solution {
     public bool WordBreak(string s, IList<string> wordDict) {
-            HashSet<string> map = new HashSet<string>(wordDict);
+        HashSet<string> map = new HashSet<string>(wordDict);
         int[] dp = new int[s.Length];
         for(int i = 0; i < s.Length; i++){
             dp[i] = -1;
         }
+        
         return Helper(s, map, 0, dp) == 1;
     }
     
-    private int Helper(string s, HashSet<string> map, int start, int[] dp){
-        if(start == s.Length){
+    private int Helper(string s, HashSet<string> map, int index, int[] dp){
+        if(index == s.Length){
             return 1;
         }
         
-        if(dp[start] != -1){
-            return dp[start];
+        if(dp[index] != -1){
+            return dp[index];
         }
         
-        for(int end = start+1; end <= s.Length; end++){
-            bool firstPart = map.Contains(s.Substring(start, end-start));
-            int secondPart = Helper(s, map, end, dp);
-            
-            if(firstPart && secondPart == 1){
-                dp[start] = 1;
+        
+        for(int i = index; i < s.Length; i++){
+            if(map.Contains(s.Substring(index,i-index+1)) && Helper(s, map, i+1, dp) == 1){
+                dp[index] = 1;
                 return 1;
             }
         }
         
-        dp[start] = 0;
-        return -1;
+        dp[index] = 0;
+        return 0;
     }
 }
