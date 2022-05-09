d9eefb1cee1bf535527dbde5d823988dd26bb581
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-09 12:21:23 +0530

    Time: 142 ms (47.30%), Space: 39.8 MB (41.59%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cs
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cs
@@ -1,16 +1,16 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-        if(nums1.Length > nums2.Length){
-            return FindMedianSortedArrays(nums2, nums1); // make sure nums1 is always the smaller one
-        }
-        
         int len1 = nums1.Length, len2 = nums2.Length;
+        
+        if(len1 > len2){
+            return FindMedianSortedArrays(nums2, nums1);
+        }
         
         int low = 0, hi = len1;
         
         while(low <= hi){
             int cut1 = low + (hi-low)/2;
-            int cut2 = (len1+len2+1)/2 - cut1;
+            int cut2 = (len1+len2+1)/2-cut1;
             
             int left1 = cut1 == 0 ? int.MinValue : nums1[cut1-1];
             int left2 = cut2 == 0 ? int.MinValue : nums2[cut2-1];
@@ -19,7 +19,8 @@ public class Solution {
             
             if(left1 <= right2 && left2 <= right1){
                 if((len1+len2)%2 == 0){
-                    return (Math.Max(left1,left2) + Math.Min(right1,right2))/2.0;
+                    double median = Math.Max(left1,left2) + Math.Min(right1,right2);
+                    return median/2.0;
                 }
                 else{
                     return Math.Max(left1,left2);
