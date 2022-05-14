cd281084952058a911566ea3b7daf5dad6dc2626
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-14 17:21:15 +0530

    Time: 23 ms (84.72%), Space: 26.3 MB (26.31%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int Tribonacci(int n) {
+        int[] dp = new int[n+1];
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        if(n <= 2){
+            return 1;
+        }
+        
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];   
+        }
+        
+        return dp[n];
+    }
+}
