c641d2c40c9eec369d3c6397d9f1cb61a1be8bbc
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-17 08:36:02 +0530

    Time: 323 ms (36.22%), Space: 45.9 MB (98.59%) - LeetHub

--- 542-01-matrix/542-01-matrix.cs
+++ 542-01-matrix/542-01-matrix.cs
@@ -1,52 +1,52 @@
 public class Solution {
     public int[][] UpdateMatrix(int[][] mat) {
-        Queue<Node> que = new Queue<Node>();
         int m = mat.Length, n = mat[0].Length;
         
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(mat[i][j] == 0){
-                    que.Enqueue(new Node(i, j, 0));
+        for(int row = 0; row < m; row++){
+            for(int col = 0; col < n; col++){
+                if(mat[row][col] != 0){
+                    //check for top
+                    int min = int.MaxValue;
+                    if(row > 0){
+                        min = Math.Min(min, mat[row-1][col]);
+                    }
+                    
+                    //check for left
+                    if(col > 0){
+                        min = Math.Min(min, mat[row][col-1]);
+                    }
+                    
+                    if(min != int.MaxValue){
+                        mat[row][col] = min+1;
+                    }
+                    else{
+                        mat[row][col] = int.MaxValue;
+                    }
                 }
             }
         }
         
-        
-        int[,] visited = new int[m,n];
-        while(que.Count > 0){
-            Node node = que.Dequeue();
-            
-            foreach(int[] dir in Directions){
-                int row = dir[0]+node.x, col = dir[1]+node.y;
-                if(row < 0 || row >= m || col < 0 || col >= n || mat[row][col] == 0 || visited[row,col] == 1){
-                    continue;
+        for(int row = m-1; row >= 0; row--){
+            for(int col = n-1; col >= 0; col--){
+                if(mat[row][col] != 0){
+                    //check for right
+                    int min = mat[row][col]-1;
+                    if(col < n-1){
+                        min = Math.Min(min, mat[row][col+1]);
+                    }
+                    
+                    //check for bottom
+                    if(row < m-1){
+                        min = Math.Min(min, mat[row+1][col]);
+                    }
+                    
+                    if(min != int.MaxValue){
+                        mat[row][col] = min+1;
+                    }
                 }
-                
-                mat[row][col] = node.dist+1;
-                visited[row,col] = 1;
-                que.Enqueue(new Node(row, col, node.dist+1));
             }
         }
         
         return mat;
-    }
-    
-    private int[][] Directions = new int[4][]{
-        new int[] { 1, 0 },
-        new int[] { -1, 0 },
-        new int[] { 0, -1 },
-        new int[] { 0, 1 }
-    };
-}
-
-public class Node{
-    public int x;
-    public int y;
-    public int dist;
-    
-    public Node(int _x, int _y, int _dist){
-        x = _x;
-        y = _y;
-        dist = _dist;
     }
 }
