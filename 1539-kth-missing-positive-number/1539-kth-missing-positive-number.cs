45a5d50c4b930b2d118e9cb6f74b4e34189a16fb
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-17 14:51:43 +0530

    Time: 96 ms (72.12%), Space: 39.4 MB (10.99%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cs
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cs
@@ -3,10 +3,12 @@ public class Solution {
         int len = arr.Length;
         
         int low = 0, hi = len-1;
+        int last = 0;
         while(low <= hi){
             int mid = low+(hi-low)/2;
             int kth = arr[mid]-(mid+1);
             if(kth < k){
+                last = mid;
                 low = mid+1;
             }
             else{
@@ -14,6 +16,7 @@ public class Solution {
             }
         }
         //Console.WriteLine(k - (arr[last]-(last+1)));
+        
         return k+low;
     }
 }
