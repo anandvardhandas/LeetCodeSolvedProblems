b2372cc2e4762d2dc703a8050af7cc951183413f
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-17 10:35:19 +0530

    Time: 153 ms (16.22%), Space: 38.8 MB (91.54%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cs
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cs
@@ -2,23 +2,20 @@ public class Solution {
     public int PeakIndexInMountainArray(int[] arr) {
         int len = arr.Length;
         
-        int i = 1;
-        while(i < len-1 && arr[i] > arr[i-1]){
-            i++;
+        int low = 0, hi = len-1;
+        while(low <= hi){
+            int mid = low+(hi-low)/2;
+            if(mid > 0 && mid < len-1 && arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(mid < len-1 && arr[mid] > arr[mid+1] && mid > 0 && arr[mid] < arr[mid-1]){
+                hi = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
         
-        int j = len-2;
-        while(j > 0 && arr[j] > arr[j+1]){
-            j--;
-        }
-        
-        i--;
-        j++;
-        if(i == j){
-            return i;
-        }
-        else{
-            return -1;
-        }
+        return low;
     }
 }
