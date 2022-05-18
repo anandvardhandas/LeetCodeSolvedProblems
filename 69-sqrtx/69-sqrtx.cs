d3b4a9d6cdfb764047821eb9ed8cbf00d5727bbf
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-18 13:09:34 +0530

    Time: 28 ms (72.92%), Space: 25.1 MB (93.13%) - LeetHub

--- 69-sqrtx/69-sqrtx.cs
+++ 69-sqrtx/69-sqrtx.cs
@@ -3,12 +3,11 @@ public class Solution {
         if(x == 0)
             return 0;
         
-        int low = 1, hi = x/2;
-        
-        int result = low;
+        int low = 1, hi = x;
+        int result = 0;
         while(low <= hi){
-            int mid = low + (hi-low)/2;
-            if((long)mid*mid > x){
+            int mid = low +(hi-low)/2;
+            if((long) mid*mid > x){
                 hi = mid-1;
             }
             else{
