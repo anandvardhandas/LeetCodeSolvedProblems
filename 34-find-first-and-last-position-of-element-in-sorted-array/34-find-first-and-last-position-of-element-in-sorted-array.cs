b86270e8e68af15582d34b7a938124a113cfbcdb
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-18 09:25:52 +0530

    Time: 209 ms (36.22%), Space: 43.4 MB (60.72%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cs
@@ -1,59 +1,48 @@
 public class Solution {
     public int[] SearchRange(int[] nums, int target) {
-        if(nums == null || nums.Length == 0){
-            return new int[] { -1, -1 };
-        }
-        
         int len = nums.Length;
-        
-        //binary search
-        int left = FindLeftMostIndex(nums, 0, len-1, target);
-        if(left == -1)
+        if(len == 0){
             return new int[] { -1, -1 };
-        int right = FindRightMostIndex(nums, 0, len-1, target);
-        if(right != -1){
-            return new int[] { left, right };
         }
+        //finding first position
+        int low = 0, hi = len-1;
         
-        return new int[] { -1, -1 };
-    }
-    
-    private int FindLeftMostIndex(int[] nums, int l, int r, int target){
-        int index = -1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(nums[mid] >= target){
-                if(nums[mid] == target){
-                    index = mid;
-                }
-                
-                r = mid-1;
+        int first = -1;
+        while(low <= hi){
+            int mid = low + (hi-low)/2;
+            if(nums[mid] < target){
+                low = mid+1;
+            }
+            else if(nums[mid] > target){
+                hi = mid-1;
             }
             else{
-                l = mid+1;
+                first = mid;
+                hi = mid-1;
             }
         }
         
+        if(first == -1){
+            return new int[] { -1, -1 };
+        }
         
-        return index;
-    }
-    
-    private int FindRightMostIndex(int[] nums, int l, int r, int target){
-        int index = -1;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(nums[mid] <= target){
-                if(nums[mid] == target){
-                    index = mid;
-                }
-                
-                l = mid+1;
+        //finding the last position
+        int last = -1;
+        low = 0; hi = len-1;
+        while(low <= hi){
+            int mid = low + (hi-low)/2;
+            if(nums[mid] < target){
+                low = mid+1;
+            }
+            else if(nums[mid] > target){
+                hi = mid-1;
             }
             else{
-                r = mid-1;
+                last = mid;
+                low = mid+1;
             }
         }
         
-        return index;
+        return new int[] { first, last };
     }
 }
