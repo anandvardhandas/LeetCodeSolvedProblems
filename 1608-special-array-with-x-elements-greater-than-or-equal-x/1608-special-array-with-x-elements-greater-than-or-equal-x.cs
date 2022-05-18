6e23e6a7d214e91abd859953d4dd2d68dde26c5a
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-18 13:34:18 +0530

    Time: 83 ms (83.93%), Space: 36.7 MB (98.21%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.cs
@@ -0,0 +1,50 @@
+public class Solution {
+    public int SpecialArray(int[] nums) {
+        int len = nums.Length;
+        
+        Array.Sort(nums);
+        
+        int low = 0, hi = nums[len-1];
+        while(low <= hi){
+            int mid = low + (hi-low)/2;
+            int pos = Helper(nums, mid);
+            //Console.WriteLine(pos);
+            int greater = len-pos-1;
+            if(pos == 0){
+                greater++;
+            }
+            if(greater == mid){
+                return mid;
+            }
+            else if(greater > mid){
+                low = mid+1;
+            }
+            else{
+                hi = mid-1;
+            }
+        }
+        
+        return -1;
+        
+    }
+    //[0,0,3,4,4]
+    private int Helper(int[] nums, int target){
+        //Console.WriteLine("target "+target);
+        int len = nums.Length-1;
+        int low = 0, hi = len-1;
+        int result = 0;
+        while(low <= hi){
+            int mid = low + (hi-low)/2;
+            if(nums[mid] >= target){
+                hi = mid-1;
+            }
+            else{
+                result = mid;
+                low = mid+1;
+            }
+        }
+        
+        //Console.WriteLine("result "+result);
+        return result;
+    }
+}
