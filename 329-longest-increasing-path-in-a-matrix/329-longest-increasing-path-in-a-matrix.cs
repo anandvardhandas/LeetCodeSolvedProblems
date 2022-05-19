6e12c7ea628a6ce6183f667ccd99ec208565d6ae
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-19 09:33:18 +0530

    Time: 145 ms (69.18%), Space: 39.4 MB (78.08%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cs
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cs
@@ -1,30 +1,21 @@
 public class Solution {
     public int LongestIncreasingPath(int[][] grid) {
         int m = grid.Length, n = grid[0].Length;
-        
         int[,] score = new int[m,n];
+        int[] maxscore = new int[] { 0 };
+        
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(score[i,j] == 0){
-                    Helper(grid, m, n, i, j, score, -1);
+                    Helper(grid, m, n, i, j, score, -1, maxscore);
                 }
             }
         }
         
-        int maxscore = 0;
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                maxscore = Math.Max(maxscore, score[i,j]);
-                //Console.Write($"{score[i,j]},");
-            }
-            
-            //Console.WriteLine("");
-        }
-        
-        return maxscore;
+        return maxscore[0];
     }
     
-    private int Helper(int[][] grid, int m, int n, int i, int j, int[,] score, int prev){
+    private int Helper(int[][] grid, int m, int n, int i, int j, int[,] score, int prev, int[] mscore){
         if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] <= prev){
             return 0;
         }
@@ -36,10 +27,11 @@ public class Solution {
         int maxscore = 0;
         foreach(int[] dir in Directions){
             int row = dir[0]+i, col = dir[1]+j;
-            maxscore = Math.Max(maxscore, Helper(grid, m, n, row, col, score, grid[i][j]));
+            maxscore = Math.Max(maxscore, Helper(grid, m, n, row, col, score, grid[i][j], mscore));
         }
         
         score[i,j] = maxscore+1;
+        mscore[0] = Math.Max(mscore[0], score[i,j]);
         return score[i,j];
     }
     
