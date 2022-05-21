4b22c386fb638aba368af34bf32bf3753656d3fb
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-21 09:20:56 +0530

    Time: 196 ms (27.54%), Space: 39.7 MB (61.70%) - LeetHub

--- 322-coin-change/322-coin-change.cs
+++ 322-coin-change/322-coin-change.cs
@@ -6,7 +6,7 @@ public class Solution {
         Array.Sort(coins);
         
         int[] dp = new int[amount+1];
-        int result = Helper(coins, amount, 0, dp);
+        int result = Helper(coins, amount, dp);
         if(result == int.MaxValue)
             return -1;
         
@@ -14,32 +14,32 @@ public class Solution {
     }
     
     
-    private int Helper(int[] coins, int amount, int used, int[] dp){
-        if(used > amount){
+    private int Helper(int[] coins, int amount, int[] dp){
+        if(amount < 0){
             return int.MaxValue;
         }
         
-        if(used == amount){
+        if(amount == 0){
             return 0;
         }
         
-        if(dp[used] > 0){
-            return dp[used];
+        if(dp[amount] > 0){
+            return dp[amount];
         }
         
         int result = int.MaxValue;
         for(int i = 0; i < coins.Length; i++){
             if(coins[i] < int.MaxValue){
-                result = Math.Min(result, Helper(coins, amount, used+coins[i], dp));
+                result = Math.Min(result, Helper(coins, amount - coins[i], dp));
             }
         }
         
         if(result == int.MaxValue){
-            dp[used] = result;
+            dp[amount] = result;
             return result;
         }
         
-        dp[used] = result+1;
+        dp[amount] = result+1;
         return result + 1;                 
     }
 }
