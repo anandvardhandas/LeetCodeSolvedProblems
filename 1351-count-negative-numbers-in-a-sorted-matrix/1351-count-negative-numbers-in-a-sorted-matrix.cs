1626fa944eb11e49e3ab23bdece21989f42e0e26
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-23 16:03:22 +0530

    Time: 180 ms (8.28%), Space: 40 MB (86.69%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.cs
@@ -1,17 +1,20 @@
 public class Solution {
     public int CountNegatives(int[][] grid) {
         int m = grid.Length, n = grid[0].Length;
-        
         int total = 0;
         
-        int col = n-1;
-        for(int i = 0; i < m; i++){
-            while(col >= 0 && grid[i][col] < 0){
-                col--;
+        int i = 0, j = n-1;
+        
+        while(i < m){
+            while(i < m && j >= 0 && grid[i][j] < 0){
+                j--;
             }
-            
-            total += n-col-1;
+
+            total += n-j-1;
+            //Console.WriteLine(j);
+            i++;
         }
+        
         
         return total;
     }
