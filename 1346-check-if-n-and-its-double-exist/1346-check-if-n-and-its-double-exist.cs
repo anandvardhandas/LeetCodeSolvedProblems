af1232707e0335a518cb668c8436421e1c486040
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-23 17:18:53 +0530

    Time: 110 ms (71.94%), Space: 41.3 MB (9.59%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cs
@@ -1,51 +1,13 @@
 public class Solution {
     public bool CheckIfExist(int[] arr) {
-        Array.Sort(arr);
+        HashSet<double> map = new HashSet<double>();
         int len = arr.Length;
-        int i = 0;
-        while(i < len && arr[i] < 0){
-            i++;
-        }
-        
-        //search forward
-        int j = i;
-        if(i < len && arr[i] == 0){
-            j = i+1;
-        }
-        
-        while(j < len){
-            bool found = BinarySearch(arr, j, len-1, 2 * arr[j]);
-            if(found)
+        for(int i = 0; i < len; i++){
+            if(map.Contains((double)2 * arr[i]) || map.Contains((double)arr[i]/2)){
                 return true;
-            
-            j++;
-        }
-        
-        //search backward
-        
-        j = i-1;
-        while(j >= 0){
-            bool found = BinarySearch(arr, 0, j, 2 * arr[j]);
-            if(found)
-                return true;
-            
-            j--;
-        }
-        
-        return false;
-    }
-    
-    private bool BinarySearch(int[] arr, int low, int hi, int target){
-        while(low <= hi){
-            int mid = low + (hi-low)/2;
-            if(arr[mid] == target){
-                return true;
-            }
-            else if(arr[mid] < target){
-                low = mid+1;
             }
             else{
-                hi = mid-1;
+                map.Add((double)arr[i]);
             }
         }
         
