6431172c32fda52a38a25f9ff2bbddbd20606c95
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-05-24 10:40:04 +0530

    Time: 87 ms (54.95%), Space: 35.8 MB (63.21%) - LeetHub

---
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cs
@@ -0,0 +1,47 @@
+public class Solution {
+    public int LongestValidParentheses(string s) {
+        int len = s.Length;
+        if(len == 0)
+            return 0;
+        
+        int open = 0, close = 0;
+        
+        int maxlen = 0;
+        
+        for(int i = 0; i < len; i++){
+            if(s[i] == '('){
+                open++;
+            }
+            else{
+                close++;
+                if(open == close){
+                    maxlen = Math.Max(maxlen, 2*close);
+                }
+                else if(close > open){
+                    open = 0;
+                    close = 0;
+                }
+            }
+        }
+        
+        open = 0;
+        close = 0;
+        for(int i = len-1; i >= 0; i--){
+            if(s[i] == ')'){
+                close++;
+            }
+            else{
+                open++;
+                if(open == close){
+                    maxlen = Math.Max(maxlen, 2*open);
+                }
+                else if(open > close){
+                    open = 0;
+                    close = 0;
+                }
+            }
+        }
+        
+        return maxlen;
+    }
+}
