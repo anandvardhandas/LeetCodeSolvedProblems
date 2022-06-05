c2affc3e1aee73884b5f69b704146731c021022f
anandvardhandas <43184689+anandvardhandas@users.noreply.github.com>
2022-06-05 09:53:07 +0530

    Time: 50 ms (23.73%), Space: 25.9 MB (37.29%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cs
+++ 52-n-queens-ii/52-n-queens-ii.cs
@@ -1,25 +1,23 @@
 public class Solution {
+    private int count;
     public int TotalNQueens(int n) {
-        List<List<Node>> result = new List<List<Node>>();
-        Helper(n, 0, new HashSet<int>(), new HashSet<int>(), new HashSet<int>(), new List<Node>(), result);
-        return result.Count;
+        Helper(n, 0, new HashSet<int>(), new HashSet<int>(), new HashSet<int>());
+        return this.count;
     }
     
-    private void Helper(int n, int row, HashSet<int> cols, HashSet<int> diag, HashSet<int> antidiag, List<Node> temp, List<List<Node>> result){
+    private void Helper(int n, int row, HashSet<int> cols, HashSet<int> diag, HashSet<int> antidiag){
         if(row == n){
-            result.Add(new List<Node>(temp));
+            this.count++;
             return;
         }
         
         for(int col = 0; col < n; col++){
             bool exists = CheckIfQueenNear(row, col, cols, diag, antidiag);
             if(!exists){
-                temp.Add(new Node(row,col));
                 cols.Add(col);
                 diag.Add(row-col);
                 antidiag.Add(row+col);
-                Helper(n, row+1, cols, diag, antidiag, temp, result);
-                temp.RemoveAt(temp.Count-1);
+                Helper(n, row+1, cols, diag, antidiag);
                 cols.Remove(col);
                 diag.Remove(row-col);
                 antidiag.Remove(row+col);
